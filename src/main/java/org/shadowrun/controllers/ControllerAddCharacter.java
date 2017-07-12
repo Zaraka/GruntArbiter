0fad81f80a91c64f77b1a31fb9db6bae9527d5a9
zaraka <zarakaangel@gmail.com>
2017-07-13 01:27:23 +0200

    add character dialog tweaks and NpEr fixes

--- src/main/java/org/shadowrun/controllers/ControllerAddCharacter.java
+++ src/main/java/org/shadowrun/controllers/ControllerAddCharacter.java
@@ -1,10 +1,7 @@
 package org.shadowrun.controllers;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.TextField;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 import org.shadowrun.common.NumericLimitListener;
 import org.shadowrun.models.Character;
@@ -38,9 +35,17 @@ public class ControllerAddCharacter {
     private CheckBox checkbox_npc;
 
     @FXML
+    private Button button_ok;
+
+    @FXML
     public void okOnAction() {
-        character = new Character(textField_name.textProperty().get(), Integer.valueOf(textField_initiative.textProperty().get()),
-                (World)world.getSelectedToggle().getUserData(), checkbox_npc.isSelected(), false, Integer.parseInt(textField_conditionMonitor.textProperty().get()));
+        character = new Character(
+                textField_name.textProperty().get(),
+                getInitative(),
+                (World) world.getSelectedToggle().getUserData(),
+                checkbox_npc.isSelected(),
+                false,
+                getConditionMonitor());
         stage.close();
     }
 
@@ -53,14 +58,26 @@ public class ControllerAddCharacter {
     public void onOpen(Stage stage) {
         this.stage = stage;
         this.character = null;
-        textField_initiative.textProperty().addListener(new NumericLimitListener(textField_initiative, 0,null));
-        textField_conditionMonitor.textProperty().addListener(new NumericLimitListener(textField_conditionMonitor, 0,null));
+        textField_initiative.textProperty().addListener(new NumericLimitListener(textField_initiative, 0, null));
+        textField_conditionMonitor.textProperty().addListener(new NumericLimitListener(textField_conditionMonitor, 0, null));
         radioButton_realWorld.setUserData(World.REAL);
         radioButton_matrix.setUserData(World.MATRIX);
         radioButton_astralPlane.setUserData(World.ASTRAL);
+
+        button_ok.disableProperty().bind(textField_name.textProperty().isEmpty());
     }
 
     public Optional<Character> getCharacter() {
         return Optional.of(character);
+    }
+
+    private int getInitative() {
+        return (textField_initiative.textProperty().isEmpty().get()) ? 0 :
+                Integer.parseInt(textField_initiative.textProperty().get());
+    }
+
+    private int getConditionMonitor() {
+        return (textField_conditionMonitor.textProperty().isEmpty().get()) ? 0 :
+                Integer.parseInt(textField_conditionMonitor.textProperty().get());
     }
 }
